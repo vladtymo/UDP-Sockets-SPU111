225c303d8ef658b75a81ee251da0e70e0f17a253
vladtymo <tymo.vlad@gmail.com>
2023-06-10 10:29:28 +0300

    Add project files.

---
+++ client_udp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client_udp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // endpoint = ip address + port (127.10.55.255:1024)
+
+            UdpClient udpClient = new UdpClient();
+
+            // create server endpoint
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("10.7.173.218"), 3344);
+
+            string message = string.Empty;
+            do
+            {
+                // write a message from keyboard
+                Console.Write("Enter a message (enter 'END' to exit): ");
+                message = Console.ReadLine();
+
+                // create byte array to send
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                // send data to the server
+                udpClient.Send(data, data.Length, endPoint);
+
+                // show server response
+                IPEndPoint serverEndPoint = null; // stores sender endpoint
+                byte[] response = udpClient.Receive(ref serverEndPoint);
+                string responseMessage = Encoding.UTF8.GetString(response);
+                Console.WriteLine($"Server response: {responseMessage} : {DateTime.Now.ToShortTimeString()}");
+
+            } while (message != "END");
+
+            Console.WriteLine("Closing the client application...");
+        }
+    }
+}
---
+++ server_udp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // create server endpoint
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("10.7.173.218"), 3344);
+
+        // create UDP socket
+        UdpClient server = new UdpClient(endPoint); // bind to endpoint
+
+        while (true)
+        {
+            Console.WriteLine("...Waiting for the request...");
+
+            IPEndPoint clientEndPoint = null; // stores sender endpoint
+
+            byte[] request = server.Receive(ref clientEndPoint); // wait until new data received
+
+            string message = Encoding.UTF8.GetString(request);
+            Console.WriteLine($"Received message: {message} : {DateTime.Now.ToShortTimeString()} from {clientEndPoint}");
+
+            // send response to the client
+            byte[] response = Encoding.UTF8.GetBytes("Thanks for the request!");
+            server.Send(response, response.Length, clientEndPoint);
+        }
+    }
+}
